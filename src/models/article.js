f03bc9cf08e0d20066e29c5be0dbf8c4b50f1da4
Eric Sampaio <ericstudy97@gmail.com>
2024-07-19 12:34:09 -0300

    ✨ feat: Created the article model.

---
+++ src/models/article.js
@@ -0,0 +1,26 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Article extends Model {
+    static associate(models) {
+      Article.hasMany(models.Comment, {
+        foreignKey: 'article_id',
+      });
+      Article.belongsTo(models.User, {
+        foreignKey: 'author_id',
+      });
+    }
+  }
+  Article.init(
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Article',
+      tableName: 'artcles',
+    },
+  );
+  return Article;
+};
