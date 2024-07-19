f2488683cf6992eb4cd03bf37ac075e842e5e61f
Eric Sampaio <ericstudy97@gmail.com>
2024-07-19 12:32:10 -0300

    ♻️ refactor: Created the association with the other models.

--- src/models/user.js
+++ src/models/user.js
@@ -2,13 +2,18 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Article, {
+        foreignKey: 'author_id',
+      });
+
+      User.hasMany(models.Notice, {
+        foreignKey: 'author_id',
+      });
+
+      User.hasMany(models.Comment, {
+        foreignKey: 'user_id',
+      });
     }
   }
   User.init(
