365b0ec14e39d3354feaeeb2ac64ed1444c674be
Eric Sampaio <ericstudy97@gmail.com>
2024-07-20 20:56:37 -0300

    ♻️ refactor: Adding the new routes in the API routes point.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const users = require('./userRoute.js');
+const articles = require('./articleRoute.js');
+const notices = require('./noticeRoute.js');
+const comments = require('./commentRoute.js');
 
 module.exports = (app) => {
-  app.use(express.json(), users);
+  app.use(express.json(), users, articles, notices, comments);
 };
