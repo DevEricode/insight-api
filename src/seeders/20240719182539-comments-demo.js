a9c76853e857423131918fe430347569e3c03616
Eric Sampaio <ericstudy97@gmail.com>
2024-07-20 10:40:34 -0300

    ✨ feat: Created seeders for comments.

---
+++ src/seeders/20240719182539-comments-demo.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'comments',
+      [
+        {
+          id: 1,
+          content: 'Great post! Very informative.',
+          author_id: 1,
+          notice_id: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          content: 'I agree with the ideas presented.',
+          author_id: 4,
+          notice_id: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 3,
+          content: 'Excellent content, thanks for sharing!',
+          author_id: 1,
+          notice_id: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 4,
+          content: 'Very useful, I hope to see more posts like this.',
+          author_id: 4,
+          notice_id: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 5,
+          content: 'Thanks for the detailed explanation.',
+          author_id: 1,
+          notice_id: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('comments', null, {});
+  },
+};
